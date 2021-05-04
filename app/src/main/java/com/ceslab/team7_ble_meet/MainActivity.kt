940a775377867bc0061685a44475809a4925d756
LUẬN VŨ DUY <tonyjjack01@gmail.com>
2021-05-04 12:27:39 +0700

    done BLE

--- app/src/main/java/com/ceslab/team7_ble_meet/MainActivity.kt
+++ app/src/main/java/com/ceslab/team7_ble_meet/MainActivity.kt
@@ -2,50 +2,68 @@ package com.ceslab.team7_ble_meet
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
 import android.bluetooth.le.*
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.ParcelUuid
 import android.text.TextUtils
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.ceslab.team7_ble_meet.ble.BleHandle
 import com.ceslab.team7_ble_meet.databinding.ActivityMainBinding
-import java.nio.charset.Charset
-import java.nio.charset.StandardCharsets
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val TAG: String = "MainActivity"
+    private val TAG = "MainActivity"
 
     companion object {
         const val PERMISSIONS_REQUEST_CODE: Int = 12
     }
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var data: String
     private lateinit var connect: BleHandle
+    // Initializes Bluetooth adapter.
+    private lateinit var bluetoothManager :BluetoothManager
+    private lateinit var bluetoothAdapter: BluetoothAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        bluetoothManager = getSystemService(BluetoothManager::class.java)
+        bluetoothAdapter = bluetoothManager.adapter
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         connect = BleHandle()
         checkPermissions()
         binding.apply {
             btnStartAdver.setOnClickListener {
-                Log.d(TAG,"Button advertise clicked")
-                data = edtData.text.toString().trim()
-                connect.advertise()
+                if (bluetoothAdapter.isEnabled) {
+                    Log.d(TAG, "BLE is enable")
+                    val data = byteArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+                    connect.advertise(data)
+                } else {
+                    Log.d(TAG, "BLE is no enable")
+                    Toast.makeText(this@MainActivity, "Turn on Bluetooth!", Toast.LENGTH_SHORT).show()
+                }
             }
             btnStartDiscover.setOnClickListener {
-                Log.d(TAG,"Button discover clicked")
-                connect.discover()
+                if (bluetoothAdapter.isEnabled) {
+                    Log.d(TAG, "BLE is enable")
+                    connect.discover()
+                } else {
+                    Log.d(TAG, "BLE is no enable")
+                    Toast.makeText(this@MainActivity, "Turn on Bluetooth!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            btnTurnOnBLE.setOnClickListener {
+                bluetoothAdapter.enable()
+                Log.d(TAG, "BLE is enable")
+                Toast.makeText(this@MainActivity, "BLE turned on!", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -53,20 +71,30 @@ class MainActivity : AppCompatActivity() {
     private fun checkPermissions() {
         val reqPermissions = ArrayList<String>()
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
+            != PackageManager.PERMISSION_GRANTED
+        ) {
             reqPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
         }
-        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
             reqPermissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
         }
         if (reqPermissions.isNotEmpty()) {
-            ActivityCompat.requestPermissions(this, reqPermissions.toTypedArray(), PERMISSIONS_REQUEST_CODE
+            ActivityCompat.requestPermissions(
+                this, reqPermissions.toTypedArray(), PERMISSIONS_REQUEST_CODE
             )
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             PERMISSIONS_REQUEST_CODE -> {
                 if ((grantResults.isNotEmpty())) {
@@ -80,5 +108,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
 }
--- app/src/main/java/com/ceslab/team7_ble_meet/ble/BleHandle.kt
+++ app/src/main/java/com/ceslab/team7_ble_meet/ble/BleHandle.kt
@@ -2,22 +2,25 @@ package com.ceslab.team7_ble_meet.ble
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.le.*
-import android.os.Build
 import android.os.ParcelUuid
 import android.util.Log
-import com.ceslab.team7_ble_meet.R
+import java.nio.charset.Charset
 import java.util.*
 
+
+@Suppress("NAME_SHADOWING")
 class BleHandle {
 
     private val HEX_CHARS: CharArray = "0123456789ABCDEF".toCharArray()
-    private val TAG: String = "advertise"
+    val TAG = "BLE_Handler"
+    private val ble_uuid: String = "00001234-0000-1000-8000-00805F9B34FB"
 
     fun init() {
 
     }
 
-    fun advertise() {
+    fun advertise(data: ByteArray) {
+        Log.d(TAG, "Advertise function called")
         val advertiser = BluetoothAdapter.getDefaultAdapter().bluetoothLeAdvertiser
         val settings = AdvertiseSettings.Builder()
             .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
@@ -25,65 +28,52 @@ class BleHandle {
             .setTimeout(0)
             .setConnectable(false)
             .build()
-        val pUuid = ParcelUuid(UUID.fromString(R.string.ble_uuid.toString()))
-        val data: AdvertiseData = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-            AdvertiseData.Builder()
-                .setIncludeDeviceName(false)
-//                         .addServiceUuid(pUuid)
-                .addServiceData(pUuid, byteArrayOf(0x01,0x02,0x03,0x04))
-                .build()
-        } else {
-            AdvertiseData.Builder()
-                .setIncludeDeviceName(false)
-//                         .addServiceUuid(pUuid)
-                .addServiceData(pUuid, byteArrayOf(0x01,0x02,0x03,0x04))
-                .build()
-        }
+        val pUuid = ParcelUuid(UUID.fromString(ble_uuid))
+        val data: AdvertiseData = AdvertiseData.Builder()
+            .setIncludeDeviceName(false)
+            .addServiceData(pUuid, data)
+            .build()
+
 
         val advertisingCallback: AdvertiseCallback = object : AdvertiseCallback() {
             override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
-                Log.d(TAG, "Advertise Successfully")
-                Log.d(TAG, data.toString())
+                Log.d(TAG, "BleHandler: Advertise Successfully")
+                Log.d(TAG, "BLEHandler: $data")
                 super.onStartSuccess(settingsInEffect)
             }
 
             override fun onStartFailure(errorCode: Int) {
-                Log.e(TAG, "Advertise Failed $errorCode")
+                Log.e(TAG, "BleHandler: Advertise Failed $errorCode")
                 super.onStartFailure(errorCode)
             }
         }
-        settings?.let{
-            Log.d(TAG, "setting is not null")
+        settings?.let {
+            Log.d(TAG, "BleHandler: setting is not null")
         }
-        Log.d(TAG, "data: $data")
-        Log.d(TAG, "advertise: $advertisingCallback")
+        Log.d(TAG, "BleHandler: data: $data")
+        Log.d(TAG, "BleHandler: advertise callback: $advertisingCallback")
         advertiser.startAdvertising(settings, data, advertisingCallback)
     }
 
     fun discover() {
+        Log.d(TAG, "Discover function called")
         val mBluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner;
         val mScanCallback: ScanCallback = object : ScanCallback() {
             override fun onScanResult(callbackType: Int, result: ScanResult) {
                 super.onScanResult(callbackType, result)
-                Log.d(TAG, "Scan result:" + bytesToHexWhitespaceDelimited(result.scanRecord?.bytes))
-
-//                if (result.device == null || TextUtils.isEmpty(result.device.name)) return
-//
-//                val builder: StringBuilder = StringBuilder(result.device.name)
-//                builder.append("\n").append(
-//                    result.scanRecord?.getServiceData(result.scanRecord!!.serviceUuids[0])?.let {
-//                        String(it, Charset.forName("UTF-8"))
-//                    }
-//                )
-//                Log.d(TAG, "Build on scan result: $builder")
-//                binding.apply {
-//                    tvDataDiscover.text = builder.toString()
-//                }
+                val adType = result.scanRecord?.bytes?.get(1)?.toInt()
+                val upper_UUID = result.scanRecord?.bytes?.get(2)?.toInt()
+                val lower_UUID = result.scanRecord?.bytes?.get(3)?.toInt()
+                if (adType == 22 && upper_UUID == 52 && lower_UUID == 18) {
+                    Log.d(
+                        TAG,
+                        "Scan result:" + bytesToHexWhitespaceDelimited(result.scanRecord?.bytes)
+                    )
+                }
             }
 
             override fun onBatchScanResults(results: List<ScanResult?>?) {
                 super.onBatchScanResults(results)
-                Log.d(TAG, "Discover successfully")
             }
 
             override fun onScanFailed(errorCode: Int) {
@@ -92,10 +82,8 @@ class BleHandle {
             }
         }
 
-//        val filter = ScanFilter.Builder().setServiceUuid(ParcelUuid(UUID.fromString(getString(R.string.ble_uuid)))).build()
-        val filter = ScanFilter.Builder().setServiceUuid(ParcelUuid(UUID.fromString(R.string.ble_uuid.toString()))).build()
-//        val filter = ScanFilter.Builder().build()
-        Log.d(TAG, "filter: ${filter.serviceUuid}")
+//        val filter = ScanFilter.Builder().setServiceUuid(ParcelUuid(UUID.fromString("00001234-0000-1000-8000-00805F9B34FB"))).build()
+        val filter = ScanFilter.Builder().build()
         val filters = listOf(filter)
         val settings = ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build()
         mBluetoothLeScanner.startScan(filters, settings, mScanCallback);
@@ -106,13 +94,12 @@ class BleHandle {
         if (value == null) {
             return ""
         }
-        val hexChars = CharArray(value.size * 3)
+        val hexChars = CharArray(value.size * 2)
         var v: Int
         for (j in value.indices) {
             v = value[j].toInt() and 0xFF
-            hexChars[j * 3] = HEX_CHARS[v ushr 4]
-            hexChars[j * 3 + 1] = HEX_CHARS[v and 0x0F]
-            hexChars[j * 3 + 2] = ' '
+            hexChars[j * 2] = HEX_CHARS[v ushr 4]
+            hexChars[j * 2 + 1] = HEX_CHARS[v and 0x0F]
         }
         return String(hexChars)
     }
